7ea8b77827de3f1ce485bb13dde305581f83b4e6
unknown <mahiramyathilake@gmail.com>
2020-10-04 15:22:49 +0530

    adminupdate

---
+++ app/src/main/java/com/example/e_channeling/AdminAddPatientFragment.java
@@ -0,0 +1,23 @@
+package com.example.e_channeling;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link AdminAddPatientFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class AdminAddPatientFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_admin_add_patient, container, false);
+    }
+}
--- app/src/main/java/com/example/e_channeling/admin_login.java
+++ app/src/main/java/com/example/e_channeling/admin_login.java
@@ -2,7 +2,9 @@ package com.example.e_channeling;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class admin_login extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class admin_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_login);
+    }
+    public void onButtonClickedLogin(View view) {
+        Intent intent = new Intent(this, admin_dashboard.class);
+        startActivity(intent);
+
     }
 }
